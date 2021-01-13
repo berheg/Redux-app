11e497c163b84a83f2e78e765699c04ae866eb7a
berheg <ink42310@yahoo.com>
2021-01-13 15:41:16 +0100

    redux functions added

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import store from './store';
+import { bugAdded, bugRemoved, bugResolved } from './actions';
+
+// state = reducer(state, action);
+//notify the subscribers
+const unsubscribe = store.subscribe(() => {
+  console.log("Store changed!", store.getState());
+})
+store.dispatch(bugAdded("Bug 1"));
+store.dispatch(bugResolved(1));
+unsubscribe();
+store.dispatch(bugRemoved(1));
+console.log(store.getState());
